a3954cf41582db2d7b00081342d97c7362976875
tomazcuber <tomazcuber@outlook.com>
2025-10-26 19:38:17 -0300

    feat(app): configure hilt application class and android main entry point

---
+++ app/src/main/java/com/tomazcuber/yourcookbok/CookbookApplication.kt
@@ -0,0 +1,7 @@
+package com.tomazcuber.yourcookbok
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CookbookApplication : Application()
--- app/src/main/java/com/tomazcuber/yourcookbok/presentation/MainActivity.kt
+++ app/src/main/java/com/tomazcuber/yourcookbok/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.tomazcuber.yourcookbok
+package com.tomazcuber.yourcookbok.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.tomazcuber.yourcookbok.ui.theme.YourCookbokTheme
+import com.tomazcuber.yourcookbok.presentation.theme.YourCookbokTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
