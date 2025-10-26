66a62c1dd04ec15ebde0019f348792de32504a9e
tomazcuber <tomazcuber@outlook.com>
2025-10-26 20:11:45 -0300

    feat(di): provide retrofit and mealdb service with hilt

--- app/src/main/java/com/tomazcuber/yourcookbok/data/Constants.kt
+++ app/src/main/java/com/tomazcuber/yourcookbok/data/Constants.kt
@@ -4,3 +4,7 @@ object DatabaseConstants {
     const val DATABASE_NAME = "recipes_database.db"
     const val RECIPE_TABLE_NAME = "saved_recipes"
 }
+
+object MealApiConstants {
+    const val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
+}
--- app/src/main/java/com/tomazcuber/yourcookbok/data/di/DataModule.kt
+++ app/src/main/java/com/tomazcuber/yourcookbok/data/di/DataModule.kt
@@ -3,13 +3,19 @@ package com.tomazcuber.yourcookbok.data.di
 import android.content.Context
 import androidx.room.Room
 import com.tomazcuber.yourcookbok.data.DatabaseConstants
+import com.tomazcuber.yourcookbok.data.MealApiConstants
 import com.tomazcuber.yourcookbok.data.local.RecipeDao
 import com.tomazcuber.yourcookbok.data.local.RecipeDatabase
+import com.tomazcuber.yourcookbok.data.remote.api.MealDbApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -17,7 +23,7 @@ import javax.inject.Singleton
 object DataModule {
     @Provides
     @Singleton
-    fun provideRecipeDatabase(@ApplicationContext context: Context) : RecipeDatabase {
+    fun provideRecipeDatabase(@ApplicationContext context: Context): RecipeDatabase {
         return Room.databaseBuilder(
             context = context,
             RecipeDatabase::class.java,
@@ -29,7 +35,28 @@ object DataModule {
 
     @Provides
     @Singleton
-    fun provideRecipeDao(recipeDatabase: RecipeDatabase) : RecipeDao {
+    fun provideRecipeDao(recipeDatabase: RecipeDatabase): RecipeDao {
         return recipeDatabase.recipeDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(json: Json): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(MealApiConstants.BASE_URL)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMealDbApiService(retrofit: Retrofit): MealDbApiService {
+        return retrofit.create(MealDbApiService::class.java)
     }
 }
