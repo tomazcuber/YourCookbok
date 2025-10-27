98c1470d0403ccbba2bd89633df58d712611b71e
tomazcuber <tomazcuber@outlook.com>
2025-10-27 18:03:47 -0300

    feat(nav): create AppNavHost for managing the apps navigation graphs

---
+++ app/src/main/java/com/tomazcuber/yourcookbok/presentation/navigation/AppNavHost.kt
@@ -0,0 +1,29 @@
+package com.tomazcuber.yourcookbok.presentation.navigation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.tomazcuber.yourcookbok.search.screen.SearchRoute
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = SearchScreen,
+        modifier = modifier
+    ) {
+        composable<SearchScreen> {
+            SearchRoute()
+        }
+        composable<SavedScreen> {
+            // Placeholder for the Saved Recipes Screen
+            Text("Saved Recipes Screen")
+        }
+    }
+}
