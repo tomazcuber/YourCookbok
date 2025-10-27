91185538ac8dfd30455025bd576470c3ec80e485
tomazcuber <tomazcuber@outlook.com>
2025-10-26 21:39:16 -0300

    chore: remove unused import on IngredientListConverter.kt

--- app/src/main/java/com/tomazcuber/yourcookbok/data/local/converter/IngredientListConverter.kt
+++ app/src/main/java/com/tomazcuber/yourcookbok/data/local/converter/IngredientListConverter.kt
@@ -2,7 +2,6 @@ package com.tomazcuber.yourcookbok.data.local.converter
 
 import androidx.room.TypeConverter
 import com.tomazcuber.yourcookbok.domain.model.Ingredient
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 class IngredientListConverter {
